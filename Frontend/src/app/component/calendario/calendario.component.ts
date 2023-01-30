a45b5379c0b6ed6cd23ff698c8d79f44fc50ad90
Kevin <angelo,espinoza@espoch,edu,ec>
2023-01-30 16:56:36 -0500

    selector de dia

--- Frontend/src/app/component/calendario/calendario.component.ts
+++ Frontend/src/app/component/calendario/calendario.component.ts
@@ -1,17 +1,19 @@
 import { getLocaleDayNames } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ComunicacionService } from 'src/app/core/service/comunicacion.service';
+import { SwtichService } from 'src/app/core/service/swtich.service';
 import { calendarioModel, dataCalendario } from 'src/app/models/calendario';
-
+import { Subject, takeUntil } from 'rxjs'
 
 @Component({
   selector: 'app-calendario',
   templateUrl: './calendario.component.html',
   styleUrls: ['./calendario.component.css']
 })
-export class CalendarioComponent implements OnInit {
-  prueba = 28
+export class CalendarioComponent implements OnInit, OnDestroy {
   
+  
+  private destroy$ = new Subject<any>()
 
 
   fechaactual = new Date();
@@ -24,15 +26,21 @@ export class CalendarioComponent implements OnInit {
   mesnumber = this.getDaysInMonth(this.anio, this.mes)
   
 
-  constructor(public srvComunicacion: ComunicacionService) { 
+  constructor(public srvComunicacion: ComunicacionService,
+              public srvSwitch: SwtichService) { 
     for (let i = 0; i<this.mesnumber; i++){
       this.diasmes[i] = i + 1;
     }
   }
-
+  
   ngOnInit(): void {
     // console.log( this.dianame);
-    this.srvComunicacion.getEntradas().subscribe({ 
+
+    
+
+    this.srvComunicacion.getEntradas()
+    .pipe(takeUntil(this.destroy$))
+    .subscribe({ 
       next: (data: calendarioModel ) => {
         this.srvComunicacion.datosEventos = data.body
         console.log(this.srvComunicacion.datosEventos);
@@ -47,8 +55,19 @@ export class CalendarioComponent implements OnInit {
     return new Date(year, month, 0).getDate();
   }
 
-  mostrarFormulario(){
-    
+  mandarDia( day: number){
+    let prueba = document.getElementById('formPadre') as any
+    if (prueba){
+      prueba.style.display = 'block'
+    }
+    this.srvSwitch.setAtrapandoDia(day)
+    // console.log(this.srvSwitch.setAtrapandoDia(day));
   }
   
+  ngOnDestroy(): void {
+    this.destroy$.next(true)
+    this.destroy$.unsubscribe()
+  }
+
+
 }
--- Frontend/src/app/component/crear-evento/crear-evento.component.ts
+++ Frontend/src/app/component/crear-evento/crear-evento.component.ts
@@ -1,22 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, OnDestroy } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ComunicacionService } from 'src/app/core/service/comunicacion.service';
 import { calendarioModel } from 'src/app/models/calendario';
-
+import { Subject, takeUntil } from 'rxjs'
+import { SwtichService } from 'src/app/core/service/swtich.service';
 
 @Component({
   selector: 'app-crear-evento',
   templateUrl: './crear-evento.component.html',
   styleUrls: ['./crear-evento.component.css']
 })
-export class CrearEventoComponent implements OnInit {
+export class CrearEventoComponent implements OnInit, OnDestroy {
 
   myForm!: FormGroup;
 
-  fechaActual = new Date()
- 
+  buenDia: number = 0
 
-  constructor(public fb: FormBuilder, public srvComunicacion: ComunicacionService) {
+  private distroy$ = new Subject<any>()
+
+  fechaActual = new Date()
+
+  @Input() diaSeleccionado!: number
+
+  constructor(public fb: FormBuilder,
+    public srvComunicacion: ComunicacionService,
+    public srvSwitch: SwtichService) {
 
     this.myForm = this.fb.group({
       titulo: ['', [Validators.required]],
@@ -25,52 +33,88 @@ export class CrearEventoComponent implements OnInit {
       mes: [0],
       anio: [0],
       lugar: [''],
-      hora: [null,[Validators.required]]
+      hora: [null, [Validators.required]]
     })
 
 
   }
 
+
   ngOnInit(): void {
+    // console.log('dia ->', this.srvSwitch.diaVolador);
+    // console.log(this.srvSwitch.atrapandoDia$);
+
+    
+
+
+
   }
 
-  crearEvento(){
-    let dia = this.fechaActual.getDate()
-    let mes = this.fechaActual.getMonth()
-    let anio = this.fechaActual.getFullYear()
 
-    this.myForm.value.dia = dia
-    this.myForm.value.mes = mes
-    this.myForm.value.anio = anio
-    console.log(this.myForm.value);
-    this.srvComunicacion.postEventos(this.myForm.value).subscribe({
-      next:(resp) =>{
-        this.obtenerEntradas()
-        console.log(resp.status);
-      } ,
-      error:(r) =>{
+
+  crearEvento() {
+    this.srvSwitch.atrapandoDia$.pipe(takeUntil(this.distroy$)).subscribe({
+
+      next: (resp: number) => {
+        this.myForm.value.dia = resp;
+        console.log(resp);
+      },
+      error: (r) => {
         console.log(r);
       }
     })
+
+
+    // let dia = this.fechaActual.getDate()
+    let mes = this.fechaActual.getMonth()
+    let anio = this.fechaActual.getFullYear()
+
+    // this.myForm.value.dia = this.srvSwitch.diaVolador
+    this.myForm.value.mes = mes
+    this.myForm.value.anio = anio
+    console.log(this.myForm.value);
+
+
+
+    this.srvComunicacion.postEventos(this.myForm.value)
+      .pipe(takeUntil(this.distroy$)) //convertimos con el pipe la variable a un takeUntil
+      .subscribe({
+        next: (resp) => {
+          this.obtenerEntradas()
+          console.log(resp);
+        },
+        error: (r) => {
+          console.log(r);
+        }
+      })
   }
 
-  obtenerEntradas(){
-    this.srvComunicacion.getEntradas().subscribe({ 
-      next: (data: calendarioModel ) => {
-        this.srvComunicacion.datosEventos = data.body
-        console.log(this.srvComunicacion.datosEventos);
-        this.myForm.reset();
-      },
-      error:(err) => {
-        console.error(err);
-      }
-    })
+  obtenerEntradas() {
+    this.srvComunicacion.getEntradas()
+      .pipe(takeUntil(this.distroy$))
+      .subscribe({
+        next: (data: calendarioModel) => {
+          this.srvComunicacion.datosEventos = data.body
+          console.log(this.srvComunicacion.datosEventos);
+          this.myForm.reset();
+        },
+        error: (err) => {
+          console.error(err);
+        }
+      })
 
   }
 
-  mostarDatos(){
+  mostarDatos() {
     this.myForm.value.titulo = this.srvComunicacion.datosEventos[8]
   }
-  
+
+  ngOnDestroy(): void {
+    //el next hace que se destruya el componente
+    this.distroy$.next(true)
+    //aqui logramos la desubscripcion
+    this.distroy$.unsubscribe()
+  }
+
 
 }
