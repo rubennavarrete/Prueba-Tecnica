0b2cce7bed698e39a918f6eae547fce648379f6c
Homero <homero6834@gmail.com>
2023-01-31 09:50:45 -0500

    Agregación de validaciones

--- Backend/src/routes/entradas.routes.js
+++ Backend/src/routes/entradas.routes.js
@@ -1,12 +1,19 @@
 import { Router } from "express";
-import { createEntrada, deleteEntrada, getEntrada, getEntradas, updateEntrada , getEntradaTres} from "../controllers/entradas.controllers.js";
+import {
+  createEntrada,
+  deleteEntrada,
+  getEntrada,
+  getEntradas,
+  updateEntrada,
+  getEntradaTres,
+} from "../controllers/entradas.controllers.js";
 const router = Router();
 
 router.get("/entradas", getEntradas);
 router.post("/entradas", createEntrada);
-router.put("/entradas/:id",updateEntrada);
-router.delete("/entradas/:id",deleteEntrada);
-router.get("/entradas/:titulo",getEntrada); 
-router.get("/tres",getEntradaTres); 
+router.put("/entradas/:id", updateEntrada);
+router.delete("/entradas/:id", deleteEntrada);
+router.get("/entradas/:titulo", getEntrada);
+router.get("/tres", getEntradaTres);
 
 export default router;
